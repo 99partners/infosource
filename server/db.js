9fc2fb882d21bf721662c85c306f97c34866515f
Anuj Patel <patel.anujp1406@gmail.com>
2025-09-12 17:33:02 +0530

    db uri

--- server/db.js
+++ server/db.js
@@ -1,7 +1,7 @@
 const mongoose = require('mongoose');
 
 async function connectToDatabase() {
-  const mongoUri = process.env.MONGO_URI || 'mongodb://127.0.0.1:27017/infosource';
+  const mongoUri = process.env.MONGO_URI;
   mongoose.set('strictQuery', true);
   await mongoose.connect(mongoUri, {
     autoIndex: true,
